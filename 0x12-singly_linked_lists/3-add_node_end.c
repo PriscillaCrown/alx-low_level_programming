3a03f4c4bc97270c77dedecf1518647aaa8b038f
PriscillaCrown <prizzytoonz@gmail.com>
2022-04-22 00:45:56 +0100

    h

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,42 +1,54 @@
 #include "lists.h"
 
 /**
- * add_node_end - adds a new node at the end
- * of a list_t list.
- * @head: head of the linked list.
- * @str: string to store in the list.
- * Return: address of the head.
+ *_strlen - length of a string
+ *@s: A pointer to an int that will be updated
+ *
+ *Return: void
  */
 
-list_t *add_node_end(list_t **head, const char *str)
+int _strlen(const char *s)
 {
-    list_t *new, *temp;
-    size_t nchar;
+	int i;
 
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-        return (NULL);
+	i = 0;
+	while (s[i] != '\0') /*Count character of string*/
+	{
+		i++;
+	}
 
-    new->str = strdup(str);
-
-    for (nchar = 0; str[nchar]; nchar++)
-        ;
-
-    new->len = nchar;
-    new->next = NULL;
-    temp = *head;
-
-    if (temp == NULL)
-    {
-        *head = new;
-    }
-    else
-    {
-        while (temp->next != NULL)
-            temp = temp->next;
-        temp->next = new;
-    }
-
-    return (*head);
+	return (i);
 }
 
+/**
+ * add_node_end -  adds a new node at the end of a list
+ * @head: pointer
+ *@str:string
+ * Return:  address of the new element
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *last_node = *head;
+	/*Using malloc*/
+	new_node = malloc(sizeof(list_t));
+
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str); /*Assing data*/
+	new_node->len = _strlen(str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (*head);
+	}
+	while (last_node->next != NULL)
+		last_node = last_node->next;
+
+	last_node->next = new_node;
+
+	return (*head);
+}
